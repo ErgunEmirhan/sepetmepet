9018546bdf21b8c5c4d337ce46d32bbaf9ec792d
msacak <msacakk@gmail.com>
2024-07-02 20:28:30 +0300

    02.07.2024 - Java15 Odev

--- src/Odev/Odev002_orta.java
+++ src/Odev/Odev002_orta.java
@@ -10,7 +10,7 @@ public class Odev002_orta {
 
         boolean isPrime = true;
 
-        for(int i=3;i<num;i+=2){
+        for(int i=3;i<num/2;i+=2){
             if(num%2==0){
                 isPrime = false;
                 break;
