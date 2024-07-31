c87e4f966a030323c9f5318361c1928601c59b6c
msacak <msacakk@gmail.com>
2024-07-31 18:06:44 +0300

    02.07.2024 - Java15 Odev

--- src/AlperenHoca/GrupProjeUygulama01/Repositories/UrunSecimSistemi.java
+++ src/AlperenHoca/GrupProjeUygulama01/Repositories/UrunSecimSistemi.java
@@ -8,6 +8,7 @@ public class UrunSecimSistemi {
     static Scanner sc = new Scanner(System.in);
     static DatabaseManager<Kiyafet> db = new DatabaseManager();
 
+
     public static void main(String[] args) {
 
        demoVeriOlustur();
@@ -19,7 +20,7 @@ public class UrunSecimSistemi {
 
     public static void demoVeriOlustur(){
         ArrayList<Kiyafet> kiyafetList = db.urunList;
-        for(int i =0;i<50;i++){
+        for(int i =0;i<5;i++){
             Random random = new Random();
             ERenk[] renkArr = ERenk.values();
             ESize[] sizeArr = ESize.values();
@@ -45,6 +46,7 @@ public class UrunSecimSistemi {
 
 
     public static void welcomeMenu() {
+        Sepet sepet = new Sepet();
         while (true) {
             System.out.println("1- Ürün listele");
             System.out.println("2- Sepete ürün ekle");
@@ -65,22 +67,76 @@ public class UrunSecimSistemi {
                    db.getAll();
 
                 }
+                case 2:{
+                    urunSec(sepet);
+
+                    break;
+                }
+                case 3:{
+
+                }
+
             }
         }
     }
 
-    public static void sizeSec(Kiyafet kiyafet) {
-        while (true) {
-            System.out.println("Urun icin size seciniz");
-            System.out.println("1- Small");
-            System.out.println("2- Medium");
-            System.out.println("3- Large");
-            System.out.print("Seciminiz: ");
+
+    public static void sepetGoruntule(Sepet sepet){
+
+    }
+    // burdan id alıp urunSecenekleri metoduna gidiyoruz.
+    public static void urunSec(Sepet sepet){
+        while(true) {
+            System.out.println("Almak istediğiniz ürün için id giriniz: ");
             int secim = sc.nextInt();
-            sc.nextLine();
+            Urun urun = db.findByID(secim);
+            if (urun == null) {
+                System.out.println("Girdiğiniz id'de kayıtlı ürün bulunmamaktadır.");
+                return;
+            }
+            urunSecenekleri(urun, sepet);
+            return;
+        }
+    }
+
+    public static void urunSecenekleri(Urun urun,Sepet sepet){
+        System.out.println("1 - Sepete ekle");
+        System.out.println("2 - Ürün detaylarını göster");
+        System.out.println("0 - Ana menüye geri dön");
+        System.out.print("Seciminiz: ");
+        int secim = sc.nextInt();
+        switch (secim){
+            case 0 :
+                System.out.println("Ana menüye dönüyorsunuz.");
+                break;
+            case 1:sepeteEkle(urun,sepet);
+            break;
 
         }
 
+
     }
 
+    public static void sepeteEkle(Urun urun,Sepet sepet){
+        System.out.println("Bu üründen "+urun.getAdet()+" adet bulunmaktadir.");
+        System.out.println("Kaç adet almak istersiniz? ");
+        int adet = sc.nextInt();
+        if(adetCheck(urun,adet)) {
+            Urun sepetUrun = new Urun(urun, adet);
+            sepet.setToplamFiyat(sepet.getToplamFiyat() + urun.getFiyat() * adet);
+            sepet.getUrunArrayList().add(urun);
+            urun.setAdet(urun.getAdet() - adet);
+            sepet.getUrunArrayList().add(sepetUrun);
+            System.out.println("Ürün sepete eklenmiştir.");
+
+        }
+        else{
+            System.out.println("Bu üründen yeterli sayida stoğumuz bulunmamaktadir.");
+        }
+
+    }
+    public static boolean adetCheck(Urun urun,int adet){
+
+        return urun.getAdet()>=adet;
+    }
 }
